46cdd10407000f50c0795eae0d172bc72a44c6dc
KiBeomLee <kibeomlee16@gmail.com>
2026-06-08 08:55:49 +0900

    first init

---
+++ src/main/java/com/example/ficc/FiccWashModelApplication.java
@@ -0,0 +1,13 @@
+package com.example.ficc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FiccWashModelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FiccWashModelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ficc/FiccWashModelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ficc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FiccWashModelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
